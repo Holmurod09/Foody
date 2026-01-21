cc7eaf8a7f3e52a433be662178d3c276c6b47a15
Organish <holmurod2009@gmail.com>
2026-01-21 09:46:44 +0500

    server

--- main/settings.py
+++ main/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 
 
@@ -115,7 +115,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # --- MEDIA FILES ---
-MEDIA_URL = ''
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
